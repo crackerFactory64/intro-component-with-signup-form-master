0da17913b578061df35ea2e44140cb1c7b9b167b
crackerFactory64 <lee.mander@gmail.com>
2021-08-06 11:53:56 +0100

    finishing touches

--- main.js
+++ main.js
@@ -8,13 +8,15 @@ document.getElementById("form__submit-button").onclick = () => {
     if (document.getElementById("form__input" + i).value == "") {
       document.getElementById("form__input-container" + i).style.borderColor =
         "hsl(0, 100%, 74%)";
-      document.getElementById("form__error-icon" + i).style.display = "block";
-      document.getElementById("form__error" + i).style.display = "block";
+      document.getElementById("form__error-icon" + i).style.visibility =
+        "visible";
+      document.getElementById("form__error" + i).style.visibility = "visible";
     } else {
       document.getElementById("form__input-container" + i).style.borderColor =
         "hsl(249, 10%, 26%)";
-      document.getElementById("form__error-icon" + i).style.display = "none";
-      document.getElementById("form__error" + i).style.display = "none";
+      document.getElementById("form__error-icon" + i).style.visibility =
+        "hidden";
+      document.getElementById("form__error" + i).style.visibility = "hidden";
     }
 
     if (
@@ -24,10 +26,10 @@ document.getElementById("form__submit-button").onclick = () => {
       document.getElementById("form__input-container3").style.borderColor =
         "hsl(0, 100%, 74%)";
       document.getElementById("form__input3").style.color = "hsl(0,100%, 74%";
-      document.getElementById("form__error-icon3").style.display = "block";
+      document.getElementById("form__error-icon3").style.visibility = "visible";
       document.getElementById("form__error3").innerHTML =
         "Looks like this in not an email";
-      document.getElementById("form__error3").style.display = "block";
+      document.getElementById("form__error3").style.visibility = "visible";
     } else if (
       document.getElementById("form__input3").value != "" &&
       isEmail(document.getElementById("form__input3").value) == true
@@ -36,8 +38,8 @@ document.getElementById("form__submit-button").onclick = () => {
         "hsl(249, 10%, 26%)";
       document.getElementById("form__input3").style.color =
         "hsl(249, 10%, 26%)";
-      document.getElementById("form__error3").style.display = "none";
-      document.getElementById("form__error-icon3").style.display = "none";
+      document.getElementById("form__error3").style.visibility = "hidden";
+      document.getElementById("form__error-icon3").style.visibility = "hidden";
     }
   }
 };
